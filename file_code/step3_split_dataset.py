b8d5cf2fc71e009645f6e2a7c692dcf232952b32
HieuLT140520 <hieulthe181031@gmail.com>
2026-05-08 14:06:03 +0700

    Update step3 split dataset and split files

--- file_code/step3_split_dataset.py
+++ file_code/step3_split_dataset.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:348cc278b21a955f2d33d0a87040978a8c37964c490418ee17612663d06f0f02
-size 6358
+oid sha256:4d887035bdac8825f18d0d0a9b22e464ed58a8bf5c5d6b98f3a8cd2a17293a89
+size 13169
